2a0c291319dd2d0c57da2ac77f0c228cb4ea7eb2
PawelLiebich <34675341+PawelLiebich@users.noreply.github.com>
2017-12-30 22:31:28 +0100

    Add files via upload

---
+++ main.ps1
@@ -0,0 +1,18 @@
+﻿Login-AzureRmAccount
+
+Set-AzureRmContext -SubscriptionID 55555555-5555-5555-5555-555555555
+
+$vault = Get-AzureRmRecoveryServicesVault -Name "55555-dev"
+Set-AzureRmRecoveryServicesVaultContext -Vault $vault
+
+$containers = Get-AzureRmRecoveryServicesBackupContainer -ContainerType AzureSQL -FriendlyName $vault.Name
+ForEach ($container in $containers) {
+ $items = Get-AzureRmRecoveryServicesBackupItem -container $container -WorkloadType AzureSQLDatabase
+ ForEach ($item in $items) {
+ Disable-AzureRmRecoveryServicesBackupProtection -item $item -RemoveRecoveryPoints -Force -ea SilentlyContinue
+ }
+ Unregister-AzureRmRecoveryServicesBackupContainer -Container $container
+}
+Remove-AzureRmRecoveryServicesVault -Vault $vault
+
+
